d013e4280ae699ffaaf17f67cdf16635deb8a22f
Olivier Meunier <oli.meunier@gmail.com>
2023-01-26 16:30:15 +0100

    Validation formulaire signup

--- 04-signup/index.php
+++ 04-signup/index.php
@@ -7,57 +7,90 @@
 
 const FILENAME = 'customers.json';
 
+$lastname = '';
+$firstname = '';
+$birthdate = '';
+$email = '';
+$newsletter = 0;
+
 // Si le formulaire a été soumis...
 if (!empty($_POST)){
 
     // 1. Récupération des données du formulaire
-    $nom = trim($_POST["lastname"]);
-    $prenom = trim($_POST["firstname"]);
-    $date_naissance = $_POST["birthdate"];
+    $lastname = trim($_POST["lastname"]);
+    $firstname = trim($_POST["firstname"]);
+    $birthdate = $_POST["birthdate"];
     $email = trim($_POST["email"]);
     $password = $_POST["password"];
     // $newsletter = $_POST["newsletter"] ?? 0;
     $newsletter = isset($_POST["newsletter"]) ? $_POST["newsletter"] : 0;
 
-    // 2. Validation des données
+    // 2. Validation des données : on initialise un tableau, on stockera les messages d'erreur dedans
+    $errors = [];
 
-
-    // Création d'un tableau associatif pour stocker les données du nouveau client
-    $newCustomer = [
-        "lastname" => $nom,
-        "firstname" => $prenom,
-        "birthdate" => $date_naissance,
-        "email" => $email,
-        "password" => $password,
-        "newsletter" => $newsletter
-    ];
-
-    // On initialise la variable $customers à un tableau vide
-    $customers = [];
-
-    // Si le fichier existe... 
-    if(file_exists(FILENAME)) {
-
-        // ... on récupère son contenu
-        $jsonData = file_get_contents(FILENAME);
-
-        // Si il y a bien des données dans le fichier JSON...
-        if($jsonData) {
-
-            // ... on les récupère !
-            $customers = json_decode($jsonData, true);
-        }
-
+    // Si le champ "firstname" n'est pas rempli...
+    if(!$firstname) {
+        $errors['firstname'] = 'Veuillez remplir le champ "Prénom"';
     }
 
-    // On ajoute le nouveau client au tableau de clients
-    array_push($customers, $newCustomer);
+    if(!$lastname) {
+        $errors['lastname'] = 'Veuillez remplir le champ "Nom"';
+    }
 
-    // conversion du tableau en chaîne JSON : PHP => JSON (string)
-    $jsonData = json_encode($customers, JSON_PRETTY_PRINT);
+    if(!$birthdate) {
+        $errors['birthdate'] = 'Veuillez remplir le champ "Date de naissance"';
+    }
 
-    // enregistrement de la chaîne JSON dans le fichier customer.json
-    file_put_contents(FILENAME, $jsonData);
+    if(!$email) {
+        $errors['email'] = 'Veuillez remplir le champ "Email"';
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = 'Veuillez remplir un email valide';
+    }
+
+    if(strlen($password) < 8) {
+        $errors['password'] = 'Le mot de passe doit comporter au moins 8 caractères';
+    }
+
+    // Si il n'y a pas d'erreur... 
+    if(empty($errors)) {
+
+        // Création d'un tableau associatif pour stocker les données du nouveau client
+        $newCustomer = [
+            "lastname" => $lastname,
+            "firstname" => $firstname,
+            "birthdate" => $birthdate,
+            "email" => $email,
+            "password" => $password,
+            "newsletter" => $newsletter
+        ];
+
+        // On initialise la variable $customers à un tableau vide
+        $customers = [];
+
+        // Si le fichier existe... 
+        if(file_exists(FILENAME)) {
+
+            // ... on récupère son contenu
+            $jsonData = file_get_contents(FILENAME);
+
+            // Si il y a bien des données dans le fichier JSON...
+            if($jsonData) {
+
+                // ... on les récupère !
+                $customers = json_decode($jsonData, true);
+            }
+
+        }
+
+        // On ajoute le nouveau client au tableau de clients
+        array_push($customers, $newCustomer);
+
+        // conversion du tableau en chaîne JSON : PHP => JSON (string)
+        $jsonData = json_encode($customers, JSON_PRETTY_PRINT);
+
+        // enregistrement de la chaîne JSON dans le fichier customer.json
+        file_put_contents(FILENAME, $jsonData);
+    }
 }
 
 ///////////////////////////////////////
