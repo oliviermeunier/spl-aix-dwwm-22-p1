e323dff2c23f21f2e665dfb88ca397e594aff658
Olivier Meunier <oli.meunier@gmail.com>
2023-01-31 16:49:12 +0100

    Signup - refactoring functions

--- 04-signup/functions.php
+++ 04-signup/functions.php
@@ -14,3 +14,106 @@ function emailExists(string $email, array $customers): bool
 
     return false;
 }
+
+function getDataFromJSON(string $filepath)
+{
+    // On initialise la variable $customers à un tableau vide
+    $datas = [];
+
+    // Si le fichier existe... 
+    if(file_exists($filepath)) {
+
+        // ... on récupère son contenu
+        $jsonData = file_get_contents($filepath);
+
+        // Si il y a bien des données dans le fichier JSON...
+        if($jsonData) {
+
+            // ... on les récupère !
+            $datas = json_decode($jsonData, true);
+        }
+    }
+
+    return $datas;
+}
+
+
+function validateForm(
+    string $lastname, 
+    string $firstname,
+    string $email,
+    string $birthdate, 
+    string $password,
+    string $passwordConfirm
+)
+{
+    // Récupération des données du fichier JSON
+    $customers = getDataFromJSON(FILENAME);
+
+    // On initialise un tableau, on stockera les messages d'erreur dedans
+    $errors = [];
+
+    // Si le champ "firstname" n'est pas rempli...
+    if(!$firstname) {
+        $errors['firstname'] = 'Veuillez remplir le champ "Prénom"';
+    }
+
+    if(!$lastname) {
+        $errors['lastname'] = 'Veuillez remplir le champ "Nom"';
+    }
+
+    if(!$birthdate) {
+        $errors['birthdate'] = 'Veuillez remplir le champ "Date de naissance"';
+    }
+
+    // Validation de l'email
+    if(!$email) {
+        $errors['email'] = 'Veuillez remplir le champ "Email"';
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = 'Veuillez remplir un email valide';
+    } elseif(emailExists($email, $customers)) {
+        $errors['email'] = 'Un compte existe déjà avec cet email';
+    }
+
+    if(strlen($password) < 8) {
+        $errors['password'] = 'Le mot de passe doit comporter au moins 8 caractères';
+    } elseif ($password != $passwordConfirm) {
+        $errors['password-confirm'] = 'La confirmation ne correspond pas';
+    }
+
+    // On retourne le tableau d'erreurs
+    return $errors;
+}
+
+
+function addCustomer(
+    string $lastname, 
+    string $firstname,
+    string $email,
+    string $birthdate, 
+    string $password,
+    int $newsletter
+)
+{
+    // Récupération des données du fichier JSON
+    $customers = getDataFromJSON(FILENAME);
+
+    // Création d'un tableau associatif pour stocker les données du nouveau client
+    $newCustomer = [
+        "lastname" => $lastname,
+        "firstname" => $firstname,
+        "birthdate" => $birthdate,
+        "email" => $email,
+        "password" => $password,
+        "newsletter" => $newsletter
+    ];
+
+    // On ajoute le nouveau client au tableau de clients
+    array_push($customers, $newCustomer);
+
+    // conversion du tableau en chaîne JSON : PHP => JSON (string)
+    $jsonData = json_encode($customers, JSON_PRETTY_PRINT);
+
+    // enregistrement de la chaîne JSON dans le fichier customer.json
+    file_put_contents(FILENAME, $jsonData);
+}
--- 04-signup/index.php
+++ 04-signup/index.php
@@ -27,79 +27,35 @@ if (!empty($_POST)){
     $password = $_POST["password"];
     $passwordConfirm = $_POST["password-confirm"];
     // $newsletter = $_POST["newsletter"] ?? 0;
-    $newsletter = isset($_POST["newsletter"]) ? $_POST["newsletter"] : 0;
+    $newsletter = isset($_POST["newsletter"]) ? intval($_POST["newsletter"]) : 0;
 
-    // 2. Validation des données : on initialise un tableau, on stockera les messages d'erreur dedans
-    $errors = [];
-
-    // Récupération des données du fichier JSON
-
-    // On initialise la variable $customers à un tableau vide
-    $customers = [];
-
-    // Si le fichier existe... 
-    if(file_exists(FILENAME)) {
-
-        // ... on récupère son contenu
-        $jsonData = file_get_contents(FILENAME);
-
-        // Si il y a bien des données dans le fichier JSON...
-        if($jsonData) {
-
-            // ... on les récupère !
-            $customers = json_decode($jsonData, true);
-        }
-    }
-
-    // Si le champ "firstname" n'est pas rempli...
-    if(!$firstname) {
-        $errors['firstname'] = 'Veuillez remplir le champ "Prénom"';
-    }
-
-    if(!$lastname) {
-        $errors['lastname'] = 'Veuillez remplir le champ "Nom"';
-    }
-
-    if(!$birthdate) {
-        $errors['birthdate'] = 'Veuillez remplir le champ "Date de naissance"';
-    }
-
-    // Validation de l'email
-    if(!$email) {
-        $errors['email'] = 'Veuillez remplir le champ "Email"';
-    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errors['email'] = 'Veuillez remplir un email valide';
-    } elseif(emailExists($email, $customers)) {
-        $errors['email'] = 'Un compte existe déjà avec cet email';
-    }
-
-    if(strlen($password) < 8) {
-        $errors['password'] = 'Le mot de passe doit comporter au moins 8 caractères';
-    } elseif ($password != $passwordConfirm) {
-        $errors['password-confirm'] = 'La confirmation ne correspond pas';
-    }
+    // Validation du formulaire
+    $errors = validateForm(
+        $lastname, 
+        $firstname,
+        $email,
+        $birthdate, 
+        $password,
+        $passwordConfirm
+    );
 
     // Si il n'y a pas d'erreur... 
     if(empty($errors)) {
 
-        // Création d'un tableau associatif pour stocker les données du nouveau client
-        $newCustomer = [
-            "lastname" => $lastname,
-            "firstname" => $firstname,
-            "birthdate" => $birthdate,
-            "email" => $email,
-            "password" => $password,
-            "newsletter" => $newsletter
-        ];
+        // Ajout du nouvel utilisateur dans le fichier JSON
+        addCustomer(
+            $lastname, 
+            $firstname,
+            $email,
+            $birthdate, 
+            $password,
+            $newsletter
+        );
 
-        // On ajoute le nouveau client au tableau de clients
-        array_push($customers, $newCustomer);
-
-        // conversion du tableau en chaîne JSON : PHP => JSON (string)
-        $jsonData = json_encode($customers, JSON_PRETTY_PRINT);
-
-        // enregistrement de la chaîne JSON dans le fichier customer.json
-        file_put_contents(FILENAME, $jsonData);
+        // Redirection vers l'index.php mais sans les données du formulaire
+        // Design pattern : POST redirect GET (cf https://fr.wikipedia.org/wiki/Post-redirect-get)
+        header('Location: index.php');
+        exit;
     }
 }
 
