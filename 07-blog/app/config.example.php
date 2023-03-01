689cb79440e6826adbc433988132d3dd3b6ff98d
Olivier Meunier <oli.meunier@gmail.com>
2023-03-01 16:51:11 +0100

    [Blog] add config.php into .gitignore

---
+++ 07-blog/app/config.example.php
@@ -0,0 +1,8 @@
+<?php  
+
+const DB_HOST = "your-host"; 
+const DB_NAME = "your-db-name"; 
+const DB_USER = "user";
+const DB_PASS = "password";
+
+const BASE_URL = 'public-path-to-your-public-directory'; 
